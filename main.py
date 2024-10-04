736c4f3cb0c8ba1fc7270ad81675f880c4147d5a
Jekyll-007 <teenofeejay@gmail.com>
2024-10-04 18:07:38 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("My name is Triumph")
+print("I like singing")
+#this is a comment
+
+#keywords in python
+print("keyword in python are....")
+import keyword
+print(keyword.kwlist)
+print(4)
+print("i am a sentence \ni am a second sentence")
